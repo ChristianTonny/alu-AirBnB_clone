5f6ff0458435676158dae357c53b3beaec8b83c1
Christian Tonny <g.iradukund1@alustudent.com>
2025-05-18 10:30:45 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -18,8 +18,7 @@ class BaseModel:
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        
+        self.updated_at = datetime.now()        
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
