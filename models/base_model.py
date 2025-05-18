2fababc50f58af638ffcdd81de8cd40533b10153
Christian Tonny <g.iradukund1@alustudent.com>
2025-05-18 10:36:31 +0200

    PEP

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,8 @@ class BaseModel:
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()        
+        self.updated_at = datetime.now()   
+
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
@@ -34,7 +35,6 @@ class BaseModel:
         """Update updated_at with current time when instance is changed"""
         self.updated_at = datetime.now()
         models.storage.save()
-
     def to_dict(self):
         """Return the dictionary of the BaseModel instance.        
         Includes the key/value pair __class__ representing
